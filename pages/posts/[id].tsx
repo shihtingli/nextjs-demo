61d07f436b4220408ea9152b9f4ce8d9c5fb0123
silvia <clarinet221@gmail.com>
2021-06-21 22:48:57 +0800

    dynamic routes

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,42 @@
+import Layout from '../../components/layout'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return (
+    <Layout home={false}>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params ? params.id : ''
+  const postData = await getPostData(id as string)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
