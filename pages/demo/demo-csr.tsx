4f22f1005f23d9c8843f1bc71af95282df5fb083
silvia <clarinet221@gmail.com>
2021-07-14 22:44:38 +0800

    add demo pages

---
+++ pages/demo/demo-csr.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function DemoCsr({ data }) {
+  const [showDate, setShowDate] = useState<string>('')
+  const getNewDateFn = async () => {
+    function delay(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms))
+    }
+    await delay(3000)
+    setShowDate(new Date().toString())
+  }
+
+  useEffect(() => {
+    getNewDateFn()
+    // preworkService.getPsList().then((data) => setPsList(data))
+  }, [])
+
+  return (
+    <Layout home={false}>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>{showDate}</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
+
+// export const getServerSideProps = async () => {
+//   function delay(ms: number) {
+//     return new Promise((resolve) => setTimeout(resolve, ms))
+//   }
+//   await delay(3000)
+//   const data = new Date().toString()
+//   return {
+//     props: {
+//       data,
+//     },
+//   }
+// }
+
+// export const getServerSideProps = async () => {
+//   // Fetch data from external API
+//   const res = await fetch(`https://.../data`)
+//   const data = await res.json()
+
+//   // Pass data to the page via props
+//   return {
+//     props: {
+//       data,
+//     },
+//   }
+// }
---
+++ pages/demo/demo-ssr.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+export default function demoSsr({ data }) {
+  return (
+    <Layout home={false}>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>{data}</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
+
+export const getServerSideProps = async () => {
+  function delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+  await delay(3000)
+  const data = new Date().toString()
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+// export const getServerSideProps = async () => {
+//   // Fetch data from external API
+//   const res = await fetch(`https://.../data`)
+//   const data = await res.json()
+
+//   // Pass data to the page via props
+//   return {
+//     props: {
+//       data,
+//     },
+//   }
+// }
